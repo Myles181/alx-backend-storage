23d7be3e26d11dca2f04e214b7bce856ae22814a
Emmanuel Cyril <cyrile450@gmail.com>
2023-08-18 16:29:24 +0000

    insert school

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -2,5 +2,6 @@
 """Insert school"""
 
 def insert_school(mongo_collection, **kwargs):
+    """Insert more collections into mongo_collection"""
     result = mongo_collection.insert_one(kwargs)
     return result.get('_id')
